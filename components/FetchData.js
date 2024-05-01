0b4fbeec1b52a12a47575279b547dbc727302345
aron <114922035+aronpaco@users.noreply.github.com>
2024-05-01 10:18:43 +0300

    refactor: function extractArticleUrls

--- components/FetchData.js
+++ components/FetchData.js
@@ -5,7 +5,8 @@ import Typography from "@mui/material/Typography";
 import {} from "path";
 
 const page = `https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss`;
-
+let articleUrls = [];
+let articleUrl;
 const FetchData = async () => {
   const res = await fetch(page, {
     headers: {
@@ -13,54 +14,38 @@ const FetchData = async () => {
     },
   });
   const html = await res.text();
-  // console.log({ html });
 
-  // const data = await Parser.parse(page, html);
-  // console.log({ data });
-  /*
-  const {
-    title,
-    author,
-    date_published,
-    dek,
-    lead_image_url,
-    content,
-    next_page_url,
-    url,
-    domain,
-    excerpt,
-    word_count,
-    direction,
-    total_pages,
-    rendered_pages,
-  } = data;
-*/
   const dom = new JSDOM(html);
   const document = dom.window.document;
   const itemElements = document.getElementsByTagName("item");
+  extractArticleUrls();
 
-  for (let i = 0; i < itemElements.length; i++) {
-    const itemElement = itemElements[i].innerHTML;
-    //const linkElement = itemElement.querySelector("link");
-    //const linkContent = linkElement.textContent;
-    const lines = itemElement.split("\n");
-    lines.forEach((line) => {
-      if (line.trim().startsWith("<link>")) {
-        line = line.replace("<link>", "");
-        console.log({ line });
-      } else if (
-        line.trim().startsWith("&lt;link&gt;") &&
-        line.trim().endsWith("&lt;/link&gt;")
-      ) {
-        line = line.replace("&lt;link&gt;", "");
-        line = line.replace("&lt;/link&gt;", "");
-        console.log({ line });
-      }
-    });
-    // console.log(lines[2]);
+  function extractArticleUrls() {
+    for (let i = 0; i < itemElements.length; i++) {
+      const itemElement = itemElements[i].innerHTML;
+      //const linkElement = itemElement.querySelector("link");
+      //const linkContent = linkElement.textContent;
+      const lines = itemElement.split("\n");
+      lines.forEach((line) => {
+        if (line.trim().startsWith("<link>")) {
+          articleUrl = line.replace("<link>", "");
+        } else if (
+          line.trim().startsWith("&lt;link&gt;") &&
+          line.trim().endsWith("&lt;/link&gt;")
+        ) {
+          articleUrl = line.replace("&lt;link&gt;", "");
+          articleUrl = line.replace("&lt;/link&gt;", "");
+        }
+        // console.log({ line });
+        articleUrls.push(articleUrl);
+        return articleUrls;
+      });
+      // console.log(lines[2]);
+    }
+
+    return articleUrls;
   }
-
-  return html;
+  return articleUrls;
 };
 
 export default FetchData;
