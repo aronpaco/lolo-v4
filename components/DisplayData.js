1f092ee5b94e5b92301f6f7ab5c650cdb6117ab3
aron <114922035+aronpaco@users.noreply.github.com>
2024-05-10 13:06:03 +0300

    design: images edge to edge, text

--- components/DisplayData.js
+++ components/DisplayData.js
@@ -24,44 +24,43 @@ const DisplayData = async () => {
             <div key={index} className="article-card">
               {article.lead_image_url && (
                 <div className="image-container">
-                  <img
-                    src={article.lead_image_url}
-                    width={200}
-                    height={200}
-                  ></img>
+                  <img src={article.lead_image_url} alt={article.title} />
                 </div>
               )}
 
-              <h2 className="article-title">
-                <a href={article.url} target="_blank" rel="noopener noreferrer">
-                  {article.title}
-                </a>
-              </h2>
+              <div className="text-content">
+                <h2 className="article-title">
+                  <a
+                    href={article.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {article.title}
+                  </a>
+                </h2>
 
-              <p className="article-meta">
-                {article.category && article.category.length > 0 && (
-                  <strong>
-                    Category:{" "}
-                    {article.category.map((category, index) => (
-                      <React.Fragment key={index}>
-                        {index > 0 && ", "}
-                        {category}
-                      </React.Fragment>
-                    ))}
-                  </strong>
-                )}
-                <br />
-                {article.author && <strong>Author: {article.author}</strong>}
-                <br />
-                {article.date_published && (
-                  <strong>Date published: {article.date_published}</strong>
-                )}
-                <br />
-              </p>
-
-              <p>
-                <strong>Word Count:</strong> {article.word_count} <br />
-              </p>
+                <p className="article-meta">
+                  {article.category && article.category.length > 0 && (
+                    <strong>
+                      Category:{" "}
+                      {article.category.map((category, index) => (
+                        <React.Fragment key={index}>
+                          {index > 0 && ", "}
+                          {category}
+                        </React.Fragment>
+                      ))}
+                    </strong>
+                  )}
+                  <br />
+                  {article.author && <strong>Author: {article.author}</strong>}
+                  <br />
+                  {article.date_published && (
+                    <strong>Date published: {article.date_published}</strong>
+                  )}
+                  <br />
+                  <strong>Word Count:</strong> {article.word_count}
+                </p>
+              </div>
             </div>
           );
         })}
