964742b2ac3999ee27ce3337661c2d6105b86127
aron <114922035+aronpaco@users.noreply.github.com>
2024-05-24 13:09:51 +0300

    feat: loading text

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,14 @@ export default async function Page({
       page?: string;
     };
   }) {
-    const page = searchParams?.page || "";
+    const page = searchParams?.page || "https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss";
 
     return(
         <div>
             <h1>Search page</h1>
             <Search placeholder="Search..."/>
             <h2>Articles</h2>
-            <Suspense>      
+            <Suspense fallback={<p>Loading feed...</p>}>      
                 <DisplayData page={page}/>
             </Suspense>
         </div>
