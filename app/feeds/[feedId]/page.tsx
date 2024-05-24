3ab5204cb28b6ddf3a4f87fb4945c7a79ddaa9fe
aron <114922035+aronpaco@users.noreply.github.com>
2024-05-24 20:57:48 +0300

    refactor: delete feeds

--- app/feeds/[feedId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function FeedDetails({
-    params,
-}: {
-    params: {feedId: string }
-}) {
-        return <h1>Feed: {params.feedId}</h1>
-}
--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function Page({
     return(
         <div>
             <h1>Lolo-v4</h1>
-            <Search placeholder="Search..."/>
+            <Search placeholder="https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss"/>
             <h2>Articles</h2>
             <Suspense fallback={<p>Loading feed...</p>}>      
                 <DisplayData page={page}/>
