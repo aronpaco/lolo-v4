3569a52fdc0d6087f1bcbcaf572573c4703a81d4
aron <114922035+aronpaco@users.noreply.github.com>
2024-06-04 09:47:27 +0300

    design: center content

--- app/page.tsx
+++ app/page.tsx
@@ -13,14 +13,17 @@ export default async function Page({
     const feedUrl = searchParams?.feedUrl || "";
 
     return(
-        <div>
-            <h1>Lolo-v4</h1>
-            <Search placeholder="https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss"/>
-            
-            <h2>Articles</h2>
-            <Suspense fallback={<p>Loading feed...</p>}>      
-                <DisplayData feedUrl={feedUrl}/>
-            </Suspense>
-        </div>
+          <body>
+            <div className="main-container">
+              <h1>Lolo-v4</h1>
+              <Search placeholder="https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss"/>
+              
+              <h2>Articles</h2>
+              <Suspense fallback={<p>Loading feed...</p>}>      
+                  <DisplayData feedUrl={feedUrl}/>
+              </Suspense>
+
+            </div>
+          </body>
     )
 }
